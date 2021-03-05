954068b184a2a12a84a0ce9e624572fa5ce94920
sathishgopu523 <prasadprabha19@gmail.com>
2021-03-05 18:17:07 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "2225c01f-e03f-4097-a618-82db3a85ab0a"
+  client_id       = "4c5cc5a9-bdee-430d-b190-7269677eab25"
+  client_secret   = "xqkx55_5V08-29AomHhQu7.-_O-Qq7iM9b"
+  tenant_id       = "81679584-b2ce-4205-86f5-49779bb73650"
+}
